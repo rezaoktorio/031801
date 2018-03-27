c3c42ec852d44bfb9c29bdd4a7487eaac854cf5c
rzldwptr <rizaldwiputra@gmail.com>
2018-03-27 22:41:44 +0700

    biar ga eror 'deprecated' aku tambahin 'error_reporting(E_ALL ^ E_DEPRECATED);' di connection.php

--- controller/connection.php
+++ controller/connection.php
@@ -3,6 +3,9 @@
     $username = "u573987905_sig";
     $password = "FZm4XZ7zcAos";
     $database = "u573987905_sig";
+
+    error_reporting(E_ALL ^ E_DEPRECATED);
+
     mysql_connect($server,$username,$password) or die("Koneksi gagal");
     mysql_select_db($database) or die("Database tidak bisa dibuka");
 ?>
