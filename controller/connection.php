c42bf8e81de1c4cd99ae19e882b4594ea6bb4b92
rzldwptr <rizaldwiputra@gmail.com>
2018-03-27 22:50:41 +0700

    masih sama

--- controller/connection.php
+++ controller/connection.php
@@ -6,6 +6,6 @@
 
     error_reporting(E_ALL ^ E_DEPRECATED);
 
-    mysql_connect($server,$username,$password) or die("Koneksi gagal");
+    mysqli_connect($server,$username,$password) or die("Koneksi gagal");
     mysql_select_db($database) or die("Database tidak bisa dibuka");
 ?>
