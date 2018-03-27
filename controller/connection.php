12f34de2029abf26ae2fc12e2f44b623ce1b280b
oktorio <oktorio@ittron-indonesia.com>
2018-03-27 23:24:44 +0700

    fixed error reporting

--- controller/connection.php
+++ controller/connection.php
@@ -5,6 +5,7 @@
     $password = "FZm4XZ7zcAos";
     $database = "u573987905_sig";
 
-    $koneksi = mysql_connect($server,$username,$password) or die("Koneksi gagal");
-    mysql_select_db($koneksi, $database) or die("Database tidak bisa dibuka");
+    error_reporting(0);
+    mysql_connect($server,$username,$password) or die("Koneksi gagal");
+    mysql_select_db($database) or die("Database tidak bisa dibuka");
 ?>
--- controller/header.php
+++ controller/header.php
@@ -76,8 +76,8 @@
     $password = "FZm4XZ7zcAos";
     $database = "u573987905_sig";
 
-    error_reporting(E_ALL ^ E_DEPRECATED);
+    error_reporting(0);
 
-    $koneksi = mysqli_connect($server,$username,$password) or die("Koneksi gagal");
-    mysqli_select_db($koneksi, $database) or die("Database tidak bisa dibuka");
+    mysql_connect($server,$username,$password) or die("Koneksi gagal");
+    mysql_select_db($database) or die("Database tidak bisa dibuka");
 ?>
