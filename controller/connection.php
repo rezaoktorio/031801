1913c98258d9539859f83250813234c061d70f1e
oktorio <oktorio@ittron-indonesia.com>
2018-03-27 22:11:08 +0700

    adding connection file

---
+++ controller/connection.php
@@ -0,0 +1,8 @@
+<?php
+    $server = "mysql.hostinger.co.id";
+    $username = "u573987905_sig";
+    $password = "FZm4XZ7zcAos";
+    $database = "u573987905_sig";
+    mysql_connect($server,$username,$password) or die("Koneksi gagal");
+    mysql_select_db($database) or die("Database tidak bisa dibuka");
+?>
