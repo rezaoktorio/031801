713e0a2159be181295b077678d75d8b51e7ac4b2
rzldwptr <rizaldwiputra@gmail.com>
2018-04-08 17:38:20 +0700

    tag name

--- controller/bottombar.php
+++ controller/bottombar.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-6">
-                © 2018. Oktorio.
+                © 2018. webplussolution.
             </div>
             <div class="col-xs-6">
                 <ul class="pull-right list-inline m-b-0">
