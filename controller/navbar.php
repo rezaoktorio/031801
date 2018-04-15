29ad7be39bfbff16c3da07ec4bfce30bc91aadec
oktorio <oktorio@ittron-indonesia.com>
2018-04-15 16:44:22 +0700

    menambah notifikasi untuk kritik & saran yang baru masuk

--- controller/navbar.php
+++ controller/navbar.php
@@ -20,7 +20,59 @@
                              <a href=""><i class="fa fa-search"></i></a>
                         </form>
                     </li>
-                    
+                    <li class="dropdown top-menu-item-xs">
+                      <?php
+                        session_start();
+
+                        //jika session username belum dibuat, atau session username kosong
+                        if (!isset($_SESSION['username']) || empty($_SESSION['username'])) {
+                          // echo "";
+                        } if (isset($_SESSION['username']) || !empty($_SESSION['username'])) {
+                          $query = mysql_query("SELECT COUNT(id_kritik) as total, subjek_kritik as subjek, isi_kritik as kritik FROM kritik WHERE tgl_kritik = CURDATE() LIMIT 0,5");
+                          $no = 1;
+                          while ($data = mysql_fetch_array($query)) {
+                            ?>
+                            <a href="#" data-target="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="true">
+                                <i class="icon-bell"></i> <span class="badge badge-xs badge-danger"><?php echo $data['total'];?></span>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-lg">
+                                <li class="notifi-title">Notifikasi Kritik & Saran</li>
+                                <li class="list-group slimscroll-noti notification-list">
+                                   <!-- list item-->
+                                   <a class="list-group-item">
+                                      <div class="media">
+                                         <div class="pull-left p-r-10">
+                                           <?php
+                                           if ($data['total'] < 1) {
+
+                                           } if ($data['total'] > 0) {
+                                             echo "<em class='fa noti-primary'>".$no."</em>";
+                                           }
+
+                                           ?>
+                                         </div>
+                                         <div class="media-body">
+                                            <h5 class="media-heading"><?php echo $data['subjek'];?></h5>
+                                            <p class="m-0">
+                                                <small><?php echo $data['kritik'];?></small>
+                                            </p>
+                                         </div>
+                                      </div>
+                                   </a>
+
+                                </li>
+                                <li>
+                                    <a href="http://031801.webplussolution.com/view/kritik" class="list-group-item text-right">
+                                        <small class="font-600">Lihat Semua</small>
+                                    </a>
+                                </li>
+                            </ul>
+                            <?php
+                          }
+                          $no++;
+                        }
+                      ?>
+                    </li>
 
                     <?php
                       session_start();
